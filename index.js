743bb6ce32208c6f3d0b94461ff91432751ae2eb
kaareltiitt6nts <kaarel-tiit.tonts@voco.ee>
2025-10-29 06:55:03 +0000

    basic server

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.listen(PORT, (err) => {
+  console.log(`Server started! http://localhost:${PORT}`);
+});
